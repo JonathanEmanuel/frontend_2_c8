5fcc35ba13bc33fd4202a43cc024108702aa9e99
Jonathan Cruz <cruzjonathan.e@gmail.com>
2023-02-13 22:26:23 -0300

    Clase 01 - Introducción JavaScript Front

--- semana_01/clase-01.js
+++ semana_01/clase-01.js
@@ -12,3 +12,10 @@ function inciarJuego(){
 }
 // Constante Global
 const nombreJugador = inciarJuego();
+
+/* -------------------------------------------------------------------------- */
+/*                          CONSIGNA MESA DE TRABAJO                          */
+/* -------------------------------------------------------------------------- */
+// 1- Modificar la funcion de iniciarJuego(), validar si ingresa un dato válido como nombre.
+// 2- Si no ingresa un texto, o tiene menos de 3 caracteres debemos volverle a pedir que lo ingrese.
+// 3- Finalmente el nombre devuelto debe estar todo en mayúsculas.
