03d04d2cda24c8276a43f0b0fae7eebb00aa4fcc
Jonathan Cruz <cruzjonathan.e@gmail.com>
2023-03-01 21:15:01 -0300

    Clase 07 - template

---
+++ semana_03/albumes.js
@@ -0,0 +1,39 @@
+/* --------------------------- listado de almbumes --------------------------- */
+const albumesFamosos = [
+  {
+    id: "x123",
+    nombre: "Nevermind",
+    imagen: "https://m.media-amazon.com/images/I/71DQrKpImPL._SL1400_.jpg",
+    like: true,
+  },
+  {
+    id: "y456",
+    nombre: "Thriller",
+    imagen:"https://img.discogs.com/LfnH5tbhcZ4xRMNLAodIyvea9xA=/fit-in/600x600/filters:strip_icc():format(webp):mode_rgb():quality(90)/discogs-images/R-294033-1151290881.jpeg.jpg",
+    like: true,
+  },
+  {
+    id: "z789",
+    nombre: "The wall",
+    imagen: "https://img.discogs.com/EbLYco6R1A-5Z7QJ4t4O1JSzsM8=/fit-in/587x600/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-4620059-1370165707-3841.jpeg.jpg",
+    like: false,
+  },
+  {
+    id: "a987",
+    nombre: "Abbey Road",
+    imagen: "https://cloudfront-us-east-1.images.arcpublishing.com/copesa/RDH5EPH2TNENPI73NBWUWWMLPA.jpg",
+    like: false,
+  },
+  {
+    id: "b654",
+    nombre: "Origin of Symmetry",
+    imagen: "https://http2.mlstatic.com/D_NQ_NP_967206-MLA26105577132_102017-O.webp",
+    like: false,
+  },
+  {
+    id: "c321",
+    nombre: "Back in Black",
+    imagen:"https://i1.wp.com/www.scienceofnoise.net/wp-content/uploads/2020/07/068660474366a63e1263e53ff370eb50.jpg",
+    like: false,
+  },
+];
