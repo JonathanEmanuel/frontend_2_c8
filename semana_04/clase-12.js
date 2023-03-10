2f7de6367c2ba6f1ae29e46a0ae2ef7695949806
Jonathan Cruz <cruzjonathan.e@gmail.com>
2023-03-09 22:16:00 -0300

    Clase 12 - Formularios

--- semana_04/clase-12.js
+++ semana_04/clase-12.js
@@ -34,3 +34,14 @@ function renderizarErrores(lista){
 }
 
 
+/* ----------------------------- MESA DE TRABAJO ---------------------------- */
+/* -------------------------------------------------------------------------- */
+/*                [5] FUNCION: Formulario completado con éxito                */
+/* -------------------------------------------------------------------------- */
+// Esta funcion se va a encargar de mostrar que el formulario se completó correctamente.
+// Para eso debera cumplir con los siguientes requerimientos.
+// 1 - Recibe el listado de errores, y solo si no hay ninguno debe:
+// 2 - mostrar al final del formulario un caja con la misma estructura que la caja de errores, pero con la tonalidad verde
+// 3 - dentro la caja debe mostrar un párrafo con el mensaje: "¡Formulario completado con éxito!"
+// 4 - a su vez se debe deshabilitar el boton del formulario
+// 5 - finalmente pasados 4 segundos: se debe eliminar esa caja, habilitar el boton y limpiar el formulario
