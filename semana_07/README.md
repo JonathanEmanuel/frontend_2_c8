d4414e2581138e1c1322e0921cc4af0028a20b90
Jonathan Cruz <cruzjonathan.e@gmail.com>
2023-03-30 22:02:21 -0300

    Clase 20 - ToDo: Cerrar sesión y obtener datos del usuario

--- semana_07/README.md
+++ semana_07/README.md
@@ -18,19 +18,19 @@ Link con la documentación de los endpoints de la API y su funcionamiento:
 
 
 ## Requerimientos funcionales en pantalla de Signup
-- [ ] FUNCIÓN 1: Escuchar el submit y preparar los datos para el envío.
-- [ ] FUNCIÓN 2: Realizar la petición a la API crear un nuevo usuario si completa todos los datos.
+- [x] FUNCIÓN 1: Escuchar el submit y preparar los datos para el envío.
+- [x] FUNCIÓN 2: Realizar la petición a la API crear un nuevo usuario si completa todos los datos.
 
 ## Requerimientos funcionales en pantalla de Login
-- [ ] FUNCIÓN 1: Escuchar el submit y preparar los datos para el envío.
-- [ ] FUNCIÓN 2: Realizar la petición a la API para ingresar si los datos del usuario son correctos.
+- [x] FUNCIÓN 1: Escuchar el submit y preparar los datos para el envío.
+- [x] FUNCIÓN 2: Realizar la petición a la API para ingresar si los datos del usuario son correctos.
 
 
 ## Requerimientos funcionales en pantalla de Tareas
-- [ ] FUNCIÓN 1: Darle funcionalidad al botón de "Cerrar sesión".
-- [ ] FUNCIÓN 2: Crear un función para obtener el nombre de usuario y presentarlo en pantalla.
+- [x] FUNCIÓN 1: Darle funcionalidad al botón de "Cerrar sesión".
+- [x] FUNCIÓN 2: Crear un función para obtener el nombre de usuario y presentarlo en pantalla.
 - [ ] FUNCIÓN 3: Implementar una función para obtener el listado de tareas.
-- [ ] FUNCIÓN 4: Lograr que funcione el botón de crear nueva tarea.
+- [p] FUNCIÓN 4: Lograr que funcione el botón de crear nueva tarea.
 - [ ] FUNCIÓN 5: Poder renderizar en pantalla las tareas según su estado, dentro del listado que corresponda.
 - [ ] FUNCIÓN 6: Cambiar una tarea pendiente a terminada y viceversa.
 - [ ] FUNCIÓN 7: Eliminar definitivamente una tarea.
--- semana_07/scripts/taks.js
+++ semana_07/scripts/taks.js
@@ -1,34 +1,52 @@
 // SEGURIDAD: Si no se encuentra en localStorage info del usuario
 // no lo deja acceder a la página, redirigiendo al login inmediatamente.
-
-
+const jwt = JSON.parse( localStorage.getItem('jwt') );
+// Si no éxiste el JWT entonce lo mando al login
+if( !jwt){
+  location.replace('index.html');
+}
 
 /* ------ comienzan las funcionalidades una vez que carga el documento ------ */
 window.addEventListener('load', function () {
 
   /* ---------------- variables globales y llamado a funciones ---------------- */
-  
+  const btnCerrarSesion = document.querySelector('#closeApp');
+  const formCrearTarea = document.querySelector('.nueva-tarea');
+  const userName = document.querySelector('.user-info p');
 
-
+  obtenerNombreUsuario();
   /* -------------------------------------------------------------------------- */
   /*                          FUNCIÓN 1 - Cerrar sesión                         */
   /* -------------------------------------------------------------------------- */
-
   btnCerrarSesion.addEventListener('click', function () {
-   
 
+    if( confirm('¿Seguro que desea Salir de la APP?')){
+      localStorage.removeItem('jwt');
+      location.replace('index.html');
 
+    }
 
   });
 
   /* -------------------------------------------------------------------------- */
   /*                 FUNCIÓN 2 - Obtener nombre de usuario [GET]                */
   /* -------------------------------------------------------------------------- */
-
   function obtenerNombreUsuario() {
    
+    const endpoint = 'https://todo-api.ctd.academy/v1/users/getMe';
+    const settings = {
+      method: 'GET',
+      headers: {
+        authorization: jwt
+      }
+    }
 
-
+    fetch( endpoint, settings )
+    .then(  resp => resp.json() )
+    .then( json => {
+        console.log(json);
+        userName.textContent = `${json.firstName}  ${json.lastName} `;
+    })
 
   };
 
@@ -36,7 +54,6 @@ window.addEventListener('load', function () {
   /* -------------------------------------------------------------------------- */
   /*                 FUNCIÓN 3 - Obtener listado de tareas [GET]                */
   /* -------------------------------------------------------------------------- */
-
   function consultarTareas() {
     
     
@@ -49,7 +66,6 @@ window.addEventListener('load', function () {
   /* -------------------------------------------------------------------------- */
   /*                    FUNCIÓN 4 - Crear nueva tarea [POST]                    */
   /* -------------------------------------------------------------------------- */
-
   formCrearTarea.addEventListener('submit', function (event) {
     
 
